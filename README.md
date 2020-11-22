9a8c34db81c5330f0a5ecdafa56dab46470009f7
Furkan İpek <developer@furkanipek.com.tr>
2020-11-22 19:46:16 +0300

    first tweet, create thread, ThreadRead

--- README.md
+++ README.md
@@ -8,3 +8,14 @@ Twitter thread reader
 | python-dotenv | >= 0.15.0 | https://github.com/theskumar/python-dotenv |
 | selenium | >= 3.141.0 | https://github.com/SeleniumHQ/selenium/ |
 | chromedriver | >= 87.0.4280.20 | https://chromedriver.chromium.org/ |
+
+## Example
+
+```python
+import main as TTR
+test = TTR.TwitterThreadReader()
+
+thread = test.ThreadRead('URL or Tweet ID')
+for i in thread:
+    print(thread[i]['full_text'])
+```
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import tweepy
-import os
+import os, re
 import sys
 from os.path import join, dirname
 from dotenv import load_dotenv
@@ -15,18 +15,41 @@ ACCESS_TOKEN_SECRET = os.getenv("ACCESS_TOKEN_SECRET")
 
 class TwitterThreadReader(object):
     def __init__(self):
+        #Tweepy
         self.Auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
         self.Auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
         self.api = tweepy.API(self.Auth)
+
         self.Tweets = []
         self.Thread = {}
+
+        self.USER = None
+        self.TweetID = None
+        self.FirstTweetID = None
+        
+        # Query APIs minimized
+        self.SearchAllTweet = {}
     
     def ParseURL(self, URL):
-        URL_Parse = URL.split('twitter.com/')[1].split('/status/')
-        self.USER = self.api.get_user(URL_Parse[0])
-        self.TweetID = URL_Parse[1]
+        TwitterUser = None
+        TweetID = None
+        m = re.search('(http|https)://twitter.com/(.*)/status/([0-9]*)', URL)
+        if m:
+            TwitterUser = m.group(2)
+            TweetID = m.group(3)
+        elif URL.isnumeric():
+            TweetID = URL
+        
+        try:
+            tweetS = self.api.get_status(id=TweetID, tweet_mode="extended")
+            TwitterUser = tweetS.user.screen_name
+            self.USER = self.api.get_user(TwitterUser)
+            self.TweetID = TweetID
+        except tweepy.TweepError as e:
+            print("[Err {}] {}".format(e.args[0][0]['code'], e.args[0][0]['message']))
 
     def GetThread(self):
+        
         from selenium import webdriver
         from selenium.webdriver.common.keys import Keys
         from selenium.webdriver.common.by import By
@@ -44,7 +67,7 @@ class TwitterThreadReader(object):
         service.start()
         driver = webdriver.Remote(service.service_url, desired_capabilities=chrome_options.to_capabilities())
         driver.implicitly_wait(10)
-        driver.get("https://twitter.com/{user}/status/{tweet_id}".format(user=self.USER.screen_name, tweet_id=self.TweetID))
+        driver.get("https://twitter.com/{user}/status/{tweet_id}".format(user=self.USER.screen_name, tweet_id=self.FirstTweetID))
         try:
             wait = WebDriverWait(driver, 10)
             element = wait.until(EC.title_contains(self.USER.name))
@@ -55,26 +78,34 @@ class TwitterThreadReader(object):
             print("Loading took too much time!")
         driver.quit()
 
+        self.Tweets.sort()
+
         return self.Tweets
     
     def CreateTread(self):
         for twt in self.Tweets:
-            tweetS = self.api.get_status(id=twt, tweet_mode="extended")
-            if tweetS.in_reply_to_status_id == None:
-                self.Thread[tweetS.id] = {'full_text', tweetS.full_text}
-            elif tweetS.in_reply_to_status_id in self.Thread.keys():
-                self.Thread[tweetS.id] = {'full_text', tweetS.full_text}
-            print(tweetS.id, tweetS.in_reply_to_status_id)
-            print(tweetS.in_reply_to_status_id, tweetS.in_reply_to_status_id in self.Thread.keys(), self.Thread.keys())
-        print(self.Thread)
+            if twt in self.SearchAllTweet.keys():
+                self.Thread[twt] = {'full_text', self.SearchAllTweet[twt]['full_text']}
+            else:    
+                tweetS = self.api.get_status(id=twt, tweet_mode="extended")
+                if tweetS.in_reply_to_status_id == None:
+                    self.Thread[tweetS.id] = {'full_text': tweetS.full_text}
+                elif tweetS.in_reply_to_status_id in self.Thread.keys():
+                    self.Thread[tweetS.id] = {'full_text': tweetS.full_text}
+        return self.Thread
 
     def GetFirstTweet(self):
         SearchTweet = self.TweetID
-        while True:
-            tweetS = self.api.get_status(id=SearchTweet)
-            print(tweetS.id, tweetS.in_reply_to_status_id)
+        while self.FirstTweetID is None:
+            tweetS = self.api.get_status(id=SearchTweet, tweet_mode="extended")
+            self.SearchAllTweet[tweetS.id] = tweetS._json
             if tweetS.in_reply_to_status_id == None:
                 self.FirstTweetID = tweetS.id
-                break
             else:
                 SearchTweet = tweetS.in_reply_to_status_id
+
+    def ThreadRead(self, URL):
+        self.ParseURL(URL)
+        self.GetFirstTweet()
+        self.GetThread()
+        return self.CreateTread()
